6cc46b9501aa7b1a155a3f69a4ae45af4191a2e8
Vance Arocho <aroc725@yahoo.com>
2025-08-09 00:31:59 -0400

    Updated HTML, CSS and JavaScript files in order to complete HW assignment

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 // script.js
 console.log("Welcome to my second Git project!");
+
+function displayCalculatedTip(tipCalculatorForm, messageElement) {
+    let billAmount = tipCalculatorForm.elements["billAmount"].value;
+    billAmount = Number(billAmount);
+    const selectedTipPercentage = tipCalculatorForm.elements["tipPercentage"].value;
+    if (selectedTipPercentage <= 0) {
+        alert("Please select a valid tip percentage!");
+        return;
+    }
+
+    const tipAmount = calculateTipAmount(billAmount, selectedTipPercentage);
+
+    const totalBill = billAmount + tipAmount;
+
+    const message = "The calculated tip amount is $" +  tipAmount + ", and the total bill is $" + totalBill + ".";
+    messageElement.innerHTML = message;
+}
+
+function calculateTipAmount(billAmount, tipPercentage) {
+    const tipAmount = (billAmount * tipPercentage) / 100;
+
+    return tipAmount;
+}
+
+function resetForm(tipCalculatorForm, messageElement) {
+    tipCalculatorForm.reset();
+    messageElement.innerHTML = '';
+
+    return;
+}
